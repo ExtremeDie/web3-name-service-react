94f842b12c5bf661f080685b4c5bd1c844e0d6b3
ExtremeDie <poringkyz05059@gmail.com>
2022-03-06 17:45:30 +0800

    added deploy.sh

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 /**
  * https://vitejs.dev/config/
  * @type { import('vite').UserConfig }
  */
 export default {
-  plugins: [reactRefresh()],
-  server: {
-    host: '0.0.0.0',
-    hmr: {
-      port: 443,
-    }
-  }
-}
+	base: "/web3-name-service-react/",
+	plugins: [reactRefresh()],
+	server: {
+		host: "0.0.0.0",
+		hmr: {
+			port: 443,
+		},
+	},
+};
